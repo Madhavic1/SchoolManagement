cb9770f66dcd6d995042ddfb35278486fa209623
Madhavi <madhavi.0108@gmail.com>
2018-04-19 21:32:04 -0700

    Student Class changes

---
+++ SchoolManagement/Grade.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement
+{
+    class Grade
+    {
+        public int Grade_Number { get; set; }
+        public int Student_Strength { get; set; }
+        public string Assigned_Teacher { get; set; }
+        public List<Student> Students { get; set;}
+        public int AnnualFee { get; set; }
+        public int Year { get; set; }
+        public  int NoOfStudents = 0;
+
+       
+        
+
+        public void addStudent(Student st)
+        {
+            Students.Add(st);
+            NoOfStudents += 1;
+        }
+
+
+
+    }
+}
--- SchoolManagement/Student.cs
+++ SchoolManagement/Student.cs
@@ -8,24 +8,49 @@ namespace SchoolManagement
 {
     class Student
     {
-        //properties  or variables
+        private static int lastStudentId = 0;
 
-        //Student ID     :   student_id
-        //student name   :   student_name
-        //Student Age    :   s_Age
-        //class or grade :   grade
-        //Student Fee paid   :   feesPaid
-        //student's Addr :   S_Addr
-        //total fee to be paid : feeTotal
+
+        #region Properties
+
+        public  int StudentId { get; private set; } //unique , can not be edited by others than School
+        public string  Name { get; private set; }
+        public int Age { get; set; }
+        public int StudentGrade { get; set; }
+        public int FeesPaid { get; private set; }
+        public string Father_Name { get; private set; }
+
+
+        #endregion
+
+
+
+        public Student()
+        {
+            FeesPaid = 0;
+            StudentId = ++lastStudentId;
+
+        }
+
+        //overlaoded constructor : Takes student ID, Student Name and his grade
+
+
+
+
+        public Student(string name,int age ,int grade,string fatherName) : this()
+        {
+           // this.student_id = id;
+            Name = name;
+            Age = age;
+            StudentGrade = grade;
+            Father_Name = fatherName;
+
+
+        }
+
+        //method to update feesPaid : student will be paying his monthly fee and so the property should get updated
+
        
-
-        public int student_id { get; set; } //unique , can not be edited by others than School
-        public string  S_Name { get; set; }
-        public int s_Age { get; set; }
-        public int grade { get; set; }
-        public float feesPaid { get; set; }
-        public float feeTotal { get; set; }
-
 
     }
 }
