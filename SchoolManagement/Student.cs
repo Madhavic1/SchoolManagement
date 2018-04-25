59f43f31a1c94387aa6993783bece89b4088a913
Madhavi <madhavi.0108@gmail.com>
2018-04-25 16:37:25 -0700

    Methods Added

--- SchoolManagement/Student.cs
+++ SchoolManagement/Student.cs
@@ -6,6 +6,15 @@ using System.Threading.Tasks;
 
 namespace SchoolManagement
 {
+
+    enum Arts
+    {
+        Music,
+        Dance,
+        Drawing,
+        Drama
+    }
+
     class Student
     {
         private static int lastStudentId = 0;
@@ -13,12 +22,13 @@ namespace SchoolManagement
 
         #region Properties
 
-        public  int StudentId { get; private set; } //unique , can not be edited by others than School
+        public  int StudentId { get; private set; } 
         public string  Name { get; private set; }
         public int Age { get; set; }
         public int StudentGrade { get; set; }
-        public int FeesPaid { get; private set; }
+        public decimal FeesPaid { get; private set; }
         public string Father_Name { get; private set; }
+        public Arts ArtSubjectTaken { get; set; }
 
 
         #endregion
@@ -50,7 +60,18 @@ namespace SchoolManagement
 
         //method to update feesPaid : student will be paying his monthly fee and so the property should get updated
 
-       
+       public void payFee(int amount)
+        {
+            FeesPaid += amount;
+
+        }
+
+        public decimal CheckAmountDue(decimal feeTotal)
+        {
+           
+                return (feeTotal - FeesPaid);
+        }
+
 
     }
 }
