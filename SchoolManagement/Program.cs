2b58e06a1f96ac80d5f6741515392350f3d3b4bb
Madhavi <madhavi.0108@gmail.com>
2018-04-28 08:27:44 -0700

    Factory class School.cs

--- SchoolManagement/Program.cs
+++ SchoolManagement/Program.cs
@@ -10,67 +10,84 @@ namespace SchoolManagement
     {
         static void Main(string[] args)
         {
+            //put everything inside a while(true) loop so that everything appears again n again to the user untill he hits quit option
 
             Console.WriteLine("Welcome to our School");
             Console.WriteLine("***********************");
-            Console.WriteLine("0 : Quit");
-            Console.WriteLine("1 : Register for School");
-            Console.WriteLine("2: View Student Information");
-            Console.WriteLine("3: Pay Tuition Fee");
-            Console.WriteLine("4: View Balance Due");
 
-
-            Console.WriteLine("Please Select an option from Above..");
-
-            var option = Console.ReadLine();
-
-            switch(option)
+            while (true)
             {
-                case "0":
-                    Console.WriteLine("Thank you for visiting ");
-                    return;
-                case "1":
-                    //Enrolling a student .. Collecting  Student's Data 
-                    Console.Write("Student Name :");
+
+
+                Console.WriteLine("0 : Quit");
+                Console.WriteLine("1 : Register for School");
+                Console.WriteLine("2: View Student Information");
+                Console.WriteLine("3: Pay Tuition Fee");
+                Console.WriteLine("4: View Balance Due");
+
+
+                Console.WriteLine("Please Select an option from Above..");
+
+                var option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "0":
+                        Console.WriteLine("Thank you for visiting ");
+                        return;
+                    case "1":
+                        //Enrolling a student .. Collecting  Student's Data 
+                        Console.Write("Student Name :");
                         var name = Console.ReadLine();
-                   
-                    Console.Write("Student Age: ");
-                        var age =Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Student Age: ");
+                        var age = Convert.ToInt32(Console.ReadLine());
 
 
-                    Console.Write(" Grade: ");
+                        Console.Write(" Grade: ");
                         var grade = Convert.ToInt32(Console.ReadLine());
 
-                    Console.WriteLine("Father Name :");
+                        Console.WriteLine("Father Name :");
                         var fatherName = Console.ReadLine();
 
-                    Console.WriteLine("Art Subject ?");
-                    var artSubjects = Enum.GetNames(typeof(Arts));
+                        Console.WriteLine("Art Subject ?");
+                        var artSubjects = Enum.GetNames(typeof(Arts));
 
-                    for(int i=0;i<artSubjects.Length;i++)
-                    {
-                        Console.WriteLine($"{i}.{artSubjects[i]}");
-                    }
-                    Console.Write("Your choice");
-                    var artSubject = (Arts)Enum.Parse(typeof(Arts), Console.ReadLine());
+                        for (int i = 0; i < artSubjects.Length; i++)
+                        {
+                            Console.WriteLine($"{i}.{artSubjects[i]}");
+                        }
+                        Console.Write("Your choice");
+                        var artSubject = (Arts)Enum.Parse(typeof(Arts), Console.ReadLine());
 
-                    var student = new Student(name,age,grade,fatherName,artSubject);
+                        var student = School.EnrollStudent(name, age, grade, fatherName, artSubject);
+                        Console.WriteLine($"N Name: {student.Name} Age: {student.Age} Grade : {student.StudentGrade} Father: {student.Father_Name} Art : {student.ArtSubjectTaken}");
 
-                    Console.ReadKey();
+                        Console.ReadKey();
 
 
 
 
-                    break;
-                case "2":
-                    break;
-                case "3":
-                    break;
-                case "4":
-                    break;
+                        break;
+                    case "2":
+                        Console.Write("Student Name :");
+                        name = Console.ReadLine();
+                        Console.WriteLine("Father Name :");
+                        fatherName = Console.ReadLine();
+                        var students = School.GetStudentInformation(name, fatherName);
+                        foreach (var std in students)
+                        {
+                            Console.WriteLine($"N Name: {std.Name} Age: {std.Age} Grade : {std.StudentGrade} Father: {std.Father_Name} Art : {std.ArtSubjectTaken}");
+                        }
+
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                }
+
             }
-
-
 
         }
     }
--- SchoolManagement/School.cs
+++ SchoolManagement/School.cs
@@ -6,9 +6,33 @@ using System.Threading.Tasks;
 
 namespace SchoolManagement
 {
-    class School
+  static  class School
     {
-        //Needs to have group of Teachers and students 
-        //some operations on fees and earned money
+
+
+        private static List<Student> students = new List<Student>();
+        //duty is to enroll a student
+        /// <summary>
+        /// This creates a new student
+        /// </summary>
+        /// <param name="name">Name of the student</param>
+        /// <param name="age">Age of student</param>
+        /// <param name="grade">Grdae in which he is studying</param>
+        /// <param name="fatherName">Student's Father name</param>
+        /// <param name="artSubject">the special art subject taken by a student</param>
+        /// <returns></returns>
+
+        public static Student EnrollStudent(string name,int age,int grade,string fatherName,Arts artSubject)
+        {
+            var student = new Student(name, age, grade, fatherName, artSubject);
+            students.Add(student);
+            return student;
+        }
+
+        public static IEnumerable<Student> GetStudentInformation(string studentName, string fatherName)
+        {
+            return students.Where(st => st.Name == studentName)
+                .Where(st => st.Father_Name == fatherName);
+        }
     }
 }
